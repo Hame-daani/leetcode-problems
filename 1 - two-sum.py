d78d6ed0a8cc5188aaa9de5bb954a89b6de13ed8
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-06-27 12:58:38 +0430

    1 done

---
+++ 1 - two-sum.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/two-sum/
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        memo = {}
+        for i, n in enumerate(nums):
+            if n in memo:
+                return memo[n], i
+            memo[target - n] = i

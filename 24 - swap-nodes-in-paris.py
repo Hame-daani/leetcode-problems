d6f48dc2824efdae7a6bc2a10add1ee326fb2dfe
Sadegh Hamedani <sad3gh.hmp@gmail.com>
2022-07-01 15:07:37 +0430

    24 done

---
+++ 24 - swap-nodes-in-paris.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/swap-nodes-in-pairs/
+
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr = head
+        while curr and curr.next:
+            curr.val, curr.next.val = curr.next.val, curr.val
+            curr = curr.next.next
+        return head
